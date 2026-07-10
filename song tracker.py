674b2c36b2a500a5d88e36e8101b0b79e3922231
drishtikarwal-wq <drishti.karwal@student.wegc.school.nz>
2026-07-10 15:59:11 +1200

    Add files via upload

--- song tracker.py
+++ song tracker.py
@@ -1,3 +1,4 @@
+
 ##
 #song tracker
 #allows to track all songs
@@ -6,19 +7,62 @@
 #06.07.26
 
 songs = []
+
 def add_songs():
-    """allows user to add song and running time"""
-    
+    """Allows user to add song and running time"""
+
     song_name = input("Enter song name: ")
-    time = int(input("Enter running time (minutes): "))
+
+    while True:
+        try:
+            time = float(input("Enter running time (minutes): "))
+            break
+        except ValueError:
+            print("Invalid choice, please enter a number.")
 
     new_song = {
         "song": song_name,
         "time": time
     }
- 
-    songs.append(new_song)
-    print("added")
 
-add_songs()
-print(songs)
+    songs.append(new_song)
+    print("Added")
+
+
+def edit_song(name, time):
+    """Allows user to edit the length of a song"""
+
+    found = False
+
+    for item in songs:
+        if item["song"].lower() == name.lower():
+            item["time"] = time
+            print("Song updated")
+            found = True
+            break
+
+    if not found:
+        print("Song not found")
+
+
+# Loop to keep menu running
+running = True
+
+while running:
+    print("\nMenu")
+    print("1. Add song")
+    print("2. Edit song")
+
+    choice = input("Enter a choice: ")
+
+    if choice == "1":
+        add_songs()
+
+    elif choice == "2":
+        song = input("Enter song name: ")
+        new_time = float(input("Enter new length of song: "))
+        edit_song(song, new_time)
+
+
+    else:
+        print("Invalid choice. Please try again.")
