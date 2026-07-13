6d4e9928d4e2fb9ed6d25154634484932bc7e27f
drishtikarwal-wq <drishti.karwal@student.wegc.school.nz>
2026-07-14 10:32:51 +1200

    Add files via upload

--- song tracker.py
+++ song tracker.py
@@ -6,6 +6,7 @@
 #17
 #06.07.26
 
+
 songs = []
 
 def add_songs():
@@ -45,24 +46,66 @@ def edit_song(name, time):
         print("Song not found")
 
 
+def delete_song(name):
+    """Allows user to delete a song"""
+
+    found = False
+
+    for i in range(len(songs)):
+        if songs[i]["song"].lower() == name.lower():
+            del songs[i]
+            print("Song deleted")
+            found = True
+            break
+
+    if not found:
+        print("Song not found")
+
+
+def view_songs():
+    """Functionality to view all songs stored in list"""
+
+    print("All songs")
+
+    for item in songs:
+        print(item)
+        
+
 # Loop to keep menu running
 running = True
 
 while running:
     print("\nMenu")
-    print("1. Add song")
-    print("2. Edit song")
+    print("A. Add song")
+    print("E. Edit song")
+    print("D. Delete song")
+    print("T. Time of songs")
+    print("P. Print all songs")
+    print("Q. Quit")
 
     choice = input("Enter a choice: ")
 
-    if choice == "1":
+    if choice == "A":
         add_songs()
 
-    elif choice == "2":
+    elif choice == "E":
         song = input("Enter song name: ")
         new_time = float(input("Enter new length of song: "))
         edit_song(song, new_time)
 
+    elif choice == "D":
+        song = input("Enter song name: ")
+        delete_song(song)
+
+    elif choice.upper() == "P":
+         view_songs()
+
+    elif choice == "Q":
+        running = False
+        print("Goodbye!")
 
     else:
         print("Invalid choice. Please try again.")
+
+        
+ 
